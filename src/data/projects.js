33228a1c409d2adbba5ad8bac589f535b61d68d7
GeniePrinie <geniexpn@gmail.com>
2023-10-06 17:19:12 +0200

    Change data file from json to js

---
+++ src/data/projects.js
@@ -0,0 +1,65 @@
+import img1 from "../images/img1.jpg";
+import img2 from "../images/img2.jpg";
+import img3 from "../images/img3.jpg";
+import img4 from "../images/img4.jpg";
+import img5 from "../images/img5.jpg";
+import img6 from "../images/img6.jpg";
+
+export const data = {
+  projectData: [
+    {
+      id: 1,
+      image: img1,
+      title: "Project 1",
+      description:
+        "An online outdoor jacket store. You can add/remove item to/from shopping cart.",
+      website: "https://cross-course-project-rainydays.netlify.app/",
+      repository:
+        "https://github.com/Noroff-FEU-Assignments/cross-course-project-GeniePrinie",
+    },
+    {
+      id: 2,
+      image: img2,
+      title: "Project 2",
+      description: "A museum website for school children and parents.",
+      website:
+        "https://semester-project-1-csm-prinyapon-prinyanut.netlify.app/",
+      repository: "https://github.com/GeniePrinie/SemesterProject_1",
+    },
+    {
+      id: 3,
+      image: img3,
+      title: "Project 3",
+      description: "A recipes blog website that uses REST api from wordpress.",
+      website: "https://living-organically.netlify.app/",
+      repository:
+        "https://github.com/Noroff-FEU-Assignments/project-exam-1-GeniePrinie",
+    },
+    {
+      id: 4,
+      image: img4,
+      title: "Project 4",
+      description:
+        "Noroff's social media site. All Noroff students and teachers can create an email and log in. Just follow the domain name instuction :)",
+      website: "https://kattalk.netlify.app/html/user/login/",
+      repository: "https://github.com/GeniePrinie/Kattalk",
+    },
+    {
+      id: 5,
+      image: img5,
+      title: "Project 5",
+      description:
+        "Noroff's market (auction site) where anyone at Noroff can sell or bid. All Noroff students and teachers can create an email and log in. Just follow the domain name instuction :)",
+      website: "https://bidbuyauction.netlify.app/src/html/listing/search/",
+      repository: "https://github.com/GeniePrinie/BidBuy",
+    },
+    {
+      id: 6,
+      image: img6,
+      title: "Project 6",
+      description: "An online shopping store created by using React",
+      website: "https://living-organically.netlify.app/",
+      repository: "https://github.com/GeniePrinie/js2-framework",
+    },
+  ],
+};
